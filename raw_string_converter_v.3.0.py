91ae08123556012eaafe98d85657671c7f5cfb55
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2023-11-11 17:42:56 +0100

    Update to raw_string_converter_v.3.0.py
    - Now, if both the variables raw_to_text and file_ are non-empty lists with the same number of elements, the conversion will be done to each file.
    - The program now is better commented, with the latest options for raw().
    - Added type checks for raw_to_text and print_, which must be bool values.

--- raw_string_converter_v.3.0.py
+++ raw_string_converter_v.3.0.py
@@ -23,6 +23,15 @@ escapes = {"\ " : "\\ ",
 ### If it's false, it will convert from text to "raw", and if it's true,
 ### it will transform a "raw" string into a "pythonic" text.
 ###
+### There is also an option to print the converted text if true (print_).
+###
+### Now, raw() has an option called file_, which lets the user convert 
+### any file's text into "raw" text (or the other way around), 
+### and then export it into another file, whose name is similar to the
+### original's.
+###
+### Also, if raw_to_text and file_ are lists, with 
+###
 
 def raw(s: str,raw_to_text: bool = False,*,print_: bool = False, file_: str = "") -> str:
     '''
@@ -30,11 +39,31 @@ def raw(s: str,raw_to_text: bool = False,*,print_: bool = False, file_: str = ""
     - From text to "raw" (False) or,
     - From "raw" to "pythonic text" (True)
     Based on the value of raw_to_text.
-    It can print the text with the variable print_.
-    If you specify a directory with file_,
+    - It can print the text with the variable print_.
+    - If you specify a directory with file_,
     you can create a copy of the file with
     "raw" or "pythonic" text.
+    - If raw_to_text and file_ are lists,
+    the conversion will be done to all of the
+    files with the respective raw_to_text
+    configuration.
     '''
+
+    if isinstance(file_, list) == True and isinstance(raw_to_text, list) == True:
+        if len(file_) != len(raw_to_text) or (len(file_) == 0 or len(raw_to_text) == 0):
+            raise SyntaxError("Both lists must have the same number of elements, and not to be empty.")
+        for i in range(len(file_)):
+            raw("",raw_to_text[i],file_ = file_[i])
+        print("The process has finished, all files were successfully created!")
+        return ""
+
+    else:
+        s, file_ = str(s), str(file_)
+        if isinstance(raw_to_text, bool) != True:
+            raise TypeError("raw_to_text must be a bool value.")
+        elif isinstance(print_, bool) != True:
+            raise TypeError("print_ must be a bool value.")
+        
     if file_ != "":
         import re
         with open(file_,"r") as f:
@@ -53,7 +82,7 @@ def raw(s: str,raw_to_text: bool = False,*,print_: bool = False, file_: str = ""
         file = open(file_, "w")     
         file.write(f"'''{s}'''")
         file.close()
-        print("File {} was created successfully!".format(file_))      
+        print("File {} was successfully created!".format(file_))      
             
     if print_ == False and file_ == "":
         return s
@@ -61,4 +90,3 @@ def raw(s: str,raw_to_text: bool = False,*,print_: bool = False, file_: str = ""
         print(str(s))
 
 # https://github.com/CyberCoral
-
